99a1bede9a29acbadf91340cd74b3f55d1d8bcd3
ola <jarco.aleksandra@gmail.com>
2025-04-16 09:35:09 +0200

    Refactor: clean up script, improve structure and readability, adding example .pdb file

--- proteinInvestigator.py
+++ proteinInvestigator.py
@@ -1,273 +1,193 @@
-'''
+#!/usr/bin/env python3
+"""
+Protein Secondary Structure Analyzer
+
 Created on Jan 21, 2014
+Author: ola
+"""
 
-@author: ola
-'''
-
-
-from Bio.PDB import PDBIO, PDBParser
+import sys
+import argparse
+from math import pi
+from Bio.PDB import PDBParser
 from Bio.PDB.PDBExceptions import PDBException
 from Bio.PDB.Polypeptide import PPBuilder, Polypeptide
-from Bio.PDB.Vector import calc_dihedral
-from argparse import ArgumentParser
-from math import pi
-import argparse
-import sys
+from Bio.PDB.vectors import calc_dihedral
+
+parser = PDBParser()
 
 
+def parse_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-pdb", dest="pdb_file", required=True, help="Path to PDB file")
+    return parser.parse_args()
 
-parser=PDBParser()
 
-def argp():
-    ''' parse arguments given by user in command line'''
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-pdb", dest="pdbFile", required=True, help="pdbFile", type=str)
-    args = parser.parse_args()
-    return args
-
-def unifyAngle(angle): #angle in degrees
-    i=1
-    while angle <-180 or angle>180:
-        if angle >180:
-            angle=angle-180*i
-        if angle <-180:
-            angle=angle+180*i
+def unify_angle(angle):
+    """Normalize angle to [-180, 180]"""
+    while angle > 180:
+        angle -= 360
+    while angle < -180:
+        angle += 360
     return angle
 
-class _PPBuilder2(PPBuilder):
-    def __init__(self):
-        PPBuilder.__init__(self)
-   
-    def build_peptides(self, entity, aa_only=1):
-        """Build and return a list of Polypeptide objects.
-       
-         @param entity: polypeptides are searched for in this object
-         @type entity: L{Structure}, L{Model} or L{Chain}
-       
-         @param aa_only: if 1, the residue needs to be a standard AA
-         @type aa_only: int
-         """
-        is_connected=self._is_connected
-        accept=self._accept
-        level=entity.get_level()
-        # Decide which entity we are dealing with
-        if level=="S":
-            model=entity[0]
-            chain_list=model.get_list()
-        elif level=="M":
-            chain_list=entity.get_list()
-        elif level=="C":
-            chain_list=[entity]
-        else:
-            raise PDBException("Entity should be Structure, Model or Chain.")
-        pp_list=[]
-        for chain in chain_list:
-            chain_it=iter(chain)
+
+class Polypeptide2(Polypeptide):
+    def get_phi_psi_list(self):
+        """Return a dictionary of phi/psi angles per residue."""
+        angles = {}
+        for i, res in enumerate(self):
             try:
-                prev_res = next(chain_it)
-                while not accept(prev_res, aa_only):
-                    prev_res = next(chain_it)
-            except StopIteration:
-                #No interesting residues at all in this chain
+                n = res['N'].get_vector()
+                ca = res['CA'].get_vector()
+                c = res['C'].get_vector()
+            except KeyError:
+                angles[res] = (None, None)
+                res.xtra["PHI"] = res.xtra["PSI"] = None
                 continue
-            pp=None
-            for next_res in chain_it:
-                if accept(prev_res, aa_only) and accept(next_res, aa_only) and is_connected(prev_res, next_res):
+
+            phi = psi = None
+
+            if i > 0:
+                try:
+                    cp = self[i - 1]['C'].get_vector()
+                    phi = calc_dihedral(cp, n, ca, c)
+                except KeyError:
+                    pass
+
+            if i < len(self) - 1:
+                try:
+                    nn = self[i + 1]['N'].get_vector()
+                    psi = calc_dihedral(n, ca, c, nn)
+                except KeyError:
+                    pass
+
+            phi_deg = unify_angle(phi * 180) if phi is not None else None
+            psi_deg = unify_angle(psi * 180) if psi is not None else None
+            angles[res] = (phi_deg, psi_deg)
+            res.xtra["PHI"], res.xtra["PSI"] = phi_deg, psi_deg
+
+        return angles
+
+
+class _PPBuilder2(PPBuilder):
+    def build_peptides(self, entity, aa_only=1):
+        """Build and return a list of Polypeptide2 objects."""
+        level = entity.get_level()
+
+        if level == "S":
+            chains = entity[0].get_list()
+        elif level == "M":
+            chains = entity.get_list()
+        elif level == "C":
+            chains = [entity]
+        else:
+            raise PDBException("Entity must be Structure, Model, or Chain.")
+
+        peptides = []
+
+        for chain in chains:
+            iterator = iter(chain)
+            try:
+                prev_res = next(iterator)
+                while not self._accept(prev_res, aa_only):
+                    prev_res = next(iterator)
+            except StopIteration:
+                continue
+
+            pp = None
+            for next_res in iterator:
+                if (self._accept(prev_res, aa_only) and self._accept(next_res, aa_only)
+                        and self._is_connected(prev_res, next_res)):
                     if pp is None:
-                        pp=Polypeptide2()
+                        pp = Polypeptide2()
                         pp.append(prev_res)
-                        pp_list.append(pp)
+                        peptides.append(pp)
                     pp.append(next_res)
                 else:
-                    #Either too far apart, or one of the residues is unwanted.
-                    #End the current peptide
-                    pp=None
-                prev_res=next_res
-        return pp_list
-       
-       
-class Polypeptide2(Polypeptide):
-    def __init__(self):
-        Polypeptide.__init__(self)
-       
-    def get_phi_psi_list(self):
-        """Return the list of phi/psi dihedral angles."""
-        ppl=[]
-        pplDict={}
-        lng=len(self)
-        for i in range(0, lng):
-            res=self[i]
-            try:
-                n=res['N'].get_vector()
-                ca=res['CA'].get_vector()
-                c=res['C'].get_vector()
-            except:
-                # Some atoms are missing
-                # Phi/Psi cannot be calculated for this residue
-                ppl.append((None, None))
-                pplDict.update({res:(None, None)})
-                res.xtra["PHI"]=None
-                res.xtra["PSI"]=None
-                continue
-            # Phi
-            if i>0:
+                    pp = None
+                prev_res = next_res
 
-                rp=self[i-1]
-                try:
-                    cp=rp['C'].get_vector()
-                    phi=calc_dihedral(cp, n, ca, c)
-                except:
-                    phi=None
-            else:
-                # No phi for residue 0!
-                phi=None
-            # Psi
-            if i<(lng-1):
-                rn=self[i+1]
-                try:
-                    nn=rn['N'].get_vector()
-                    psi=calc_dihedral(n, ca, c, nn)
-                except:
-                    psi=None
-            else:
-                # No psi for last residue!
-                psi=None
-            ppl.append((phi, psi))
-            pplDict.update({res:(phi, psi)})
-            # Add Phi/Psi to xtra dict of residue
-            res.xtra["PHI"]=phi
-            res.xtra["PSI"]=psi
-        return pplDict 
+        return peptides
 
-class protein_investigation():
-    def __init__(self,resNr,chainName,pdbFile):
-        self.resNr=resNr
-        self.chainName=chainName
-        self.pdbFile=pdbFile
-        self.result =False
-        #structure=self.structure
-        #self.structure_file = structure_file
-        self.structure=parser.get_structure(pdbFile,pdbFile)
-   
-    def ifChainInStructure(self):
-        result=False
-        self.chain=[chain for chain in self.structure[0].child_list if chain.id==self.chainName]
-        if self.chain:
-            result= True
-        return result
-   
-       
+
+class ProteinInvestigation:
+    def __init__(self, res_nr, chain_name, pdb_file):
+        self.resNr = res_nr
+        self.chainName = chain_name
+        self.pdbFile = pdb_file
+        self.structure = parser.get_structure(pdb_file, pdb_file)
+        self.chain = None
+        self.residue = None
+
+    def if_chain_in_structure(self):
+        self.chain = [c for c in self.structure[0] if c.id == self.chainName]
+        return bool(self.chain)
+
     def if_structure_in_file(self):
-        #try:
-        structure=parser.get_structure(self.pdbFile,self.pdbFile)
-        if len([a for a in structure.get_residues()])!=0:
-            self.result= True
-           
-        #print self.result
-        return self.result   
-   
-    def findResInStructure(self):
-        self.residue=False
-     
-        b=False
-        if (self.if_structure_in_file() and self.ifChainInStructure()) ==True:
-       
-            structure=parser.get_structure(self.pdbFile,self.pdbFile)
-       
-            try:
-                self.residue =[a for a in structure.get_residues() if a.id[1]==int(self.resNr)][0]
-                b=True
-            except:
-                pass
-      
-        if b==False:
-            print 'can not find residue in structure'
+        return bool([r for r in self.structure.get_residues()])
+
+    def find_res_in_structure(self):
+        if not self.if_structure_in_file() or not self.if_chain_in_structure():
+            print('Cannot find residue in structure.')
             sys.exit()
-        else: 
-            #print b 
-            #print self.residue
-            return self.residue
-       
-    def CalcDihedral(self,atom1,atom2,atom3,atom4):
-        if self.findResInStructure():
-            vector1=atom1.get_vector()
-            vector2=atom2.get_vector()
-            vector3=atom3.get_vector()
-            vector4=atom4.get_vector()
-            #print vector1, vector2,vector3,vector4
-            angle=calc_dihedral(vector1, vector2, vector3, vector4)*180/pi
-            #print angle
-            return angle
-        else:
-            return False
-           
- 
-    def defineSs(self,polipeptide): #define secondary structure
-        phiPsidict=polipeptide.get_phi_psi_list()
-       
-       
-        dictSs={}
-        for (residue,phiPsi) in [(k,v) for (k,v) in phiPsidict.iteritems()]:
-            (phi,psi)=phiPsi
-            ss='U'
-            if phi !=None:
-                #phi=phi*180
-                phi=unifyAngle(phi*180)
-           
-            if psi !=None:
-                #psi=psi*180
-                psi=unifyAngle(psi*180)
-               
-            if -145<phi<-135 and 130<psi<140:
-                ss='b_anti'
-                #print ss
-            if  -125<phi<-115 and 110<psi<120:
-                ss='b_par'
-                #print residue.resname, residue.id[1], ss
-            if -180<phi<-100 and 100<psi<180:
-                ss='betha'
-                #print residue.resname,residue.id[1], ss
-            if -135<phi<-45 and-50<psi<40:
-                ss='alpha'
-                #print residue.resname, residue.id[1],ss
-            #else:
-                #ss='U'
-                #print residue.resname,residue.id[1], ss
-           
-            print residue.resname,residue.id[1],ss,phi,psi
-           
-        pass
-           
-ppb=_PPBuilder2()
+
+        try:
+            self.residue = [
+                r for r in self.structure.get_residues()
+                if r.id[1] == int(self.resNr)
+            ][0]
+        except IndexError:
+            print('Residue not found.')
+            sys.exit()
+
+        return self.residue
+
+    def calc_dihedral(self, atom1, atom2, atom3, atom4):
+        self.find_res_in_structure()
+        angle = calc_dihedral(
+            atom1.get_vector(),
+            atom2.get_vector(),
+            atom3.get_vector(),
+            atom4.get_vector()
+        ) * 180 / pi
+        return angle
+
+    def define_ss(self, polypeptide):
+        """Define secondary structure based on phi/psi angles."""
+        phi_psi_dict = polypeptide.get_phi_psi_list()
+
+        for residue, (phi, psi) in phi_psi_dict.items():
+            ss = 'U'
+            if phi is not None and psi is not None:
+                if -145 < phi < -135 and 130 < psi < 140:
+                    ss = 'b_anti'
+                elif -125 < phi < -115 and 110 < psi < 120:
+                    ss = 'b_par'
+                elif -180 < phi < -100 and 100 < psi < 180:
+                    ss = 'beta'
+                elif -135 < phi < -45 and -50 < psi < 40:
+                    ss = 'alpha'
+            print(residue.resname, residue.id[1], ss, phi, psi)
+
+
 def main():
-   
-   
-    #print unifyAngle(1200)
-    #sys.exit()
-    pdbFile=argp().pdbFile
-    structure=parser.get_structure(pdbFile,pdbFile)
-    chainA=[a for a in structure.get_chains() if a.id=='A'][0]
-    polipeptide=ppb.build_peptides(chainA,1)[0]
-    #print polipeptide
-    #sys.exit()
-    resNr=238
-    chainName='A'
-    #peptide = [res for res in structure.get_residues()][0:4]
-   
-    #print peptide
-    #[atom1,atom2,atom3,atom4]=[a for a in structure.get_atoms()][0:4]
-    #print [atom1,atom2,atom3,atom4]
-    #print  [atom1.get_coord(),atom2.get_coord(),atom3.get_coord(),atom4.get_coord()] 
-    #residue=parser.get_structure(pdbFile,pdbFile)
-    PI= protein_investigation(resNr,chainName,pdbFile)
-    #PI.CalcDihedral(atom1, atom2, atom3, atom4)
-    PI.defineSs(polipeptide)
-   
-   
+    # args = parse_args()
+    pdb_file = 'examples/9lil.pdb'  # Hardcoded for now
+    chain_name = 'A'
+    res_nr = 238
+
+    structure = parser.get_structure(pdb_file, pdb_file)
+    chain_a = [c for c in structure.get_chains() if c.id == chain_name][0]
+
+    ppb = _PPBuilder2()
+    peptide = ppb.build_peptides(chain_a, aa_only=1)[0]
+
+    PI = ProteinInvestigation(res_nr, chain_name, pdb_file)
+    PI.define_ss(peptide)
+
 
 if __name__ == '__main__':
     main()
-
-
-if __name__ == '__main__':
-    pass

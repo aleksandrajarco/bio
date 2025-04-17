6371ab49693a8bf767ba7b31a4b7e11dcad4c836
ola <jarco.aleksandra@gmail.com>
2025-04-17 09:15:00 +0200

    Refactor main.py to use command-line arguments for residue and chain selection

--- main.py
+++ main.py
@@ -5,25 +5,21 @@ from Bio.PDB import PDBParser
 from advanced_polypeptide_builder import DihedralPPBuilder
 from residue_analyzer import ResidueAnalyzer
 
-
 parser = PDBParser()
-
 
 def parse_args():
     """Parse command-line arguments."""
     arg_parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     arg_parser.add_argument("-pdb", dest="pdb_file", required=True, help="Path to PDB file")
+    arg_parser.add_argument("-res", dest="residue_number", required=False, help="Residue number")
+    arg_parser.add_argument("-chain", dest="chain_name", required=False,  default = 'A',  help="Chain name")
     return arg_parser.parse_args()
 
-
 def main():
-    # args = parse_args()
-
-    pdb_file = 'examples/9lil.pdb'  # Hardcoded for now
-    chain_name = 'A'
-    res_nr = 238
-
+    args = parse_args()
+    pdb_file = args.pdb_file
+    chain_name = args.chain_name
     structure = parser.get_structure(pdb_file, pdb_file)
     chain_a = [c for c in structure.get_chains() if c.id == chain_name][0]
 
